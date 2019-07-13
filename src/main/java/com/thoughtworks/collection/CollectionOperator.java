f4e3f49a6a5e269f5ff37fdf677355c3a6df7cac
Paulljc <reborn_cheng@126.com>
2019-07-13 11:32:29 +0800

    complete collection pratice

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,23 +2,44 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        if (left < right) {
+            IntStream.rangeClosed(left, right).forEach(result::add);
+            return result;
+        } else {
+            IntStream.rangeClosed(right, left).forEach(result::add);
+            return result.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        }
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        if (left < right) {
+            IntStream.rangeClosed(left, right).filter(item -> item % 2 == 0).forEach(result::add);
+            return result;
+        } else {
+            IntStream.rangeClosed(right, left).filter(item -> item % 2 == 0).forEach(result::add);
+            return result.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        }
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        Arrays.stream(array).filter(item -> item % 2 == 0).forEach(result::add);
+        return result;
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        return IntStream.of(array).skip(array.length - 1).boxed().collect(Collectors.toList()).get(0);
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
